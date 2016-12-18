fe8db4405af34e103f743e63a7aa64e9b453d4ae
jewbre <jewbre18@gmail.com>
2016-12-18 16:45:18 +0100

    initial commit

---
+++ cmake-build-debug/CMakeFiles/bioinf.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bioinf.dir/main.cpp.o"
+  "bioinf.pdb"
+  "bioinf"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bioinf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
